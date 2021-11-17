75f6d96092291d0d313704d6233d86a4da19323e
zakaria29 <zakaria29@smktelkom-mlg.sch.id>
2021-11-17 10:36:09 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,19 @@ import logo from "../assets/logo.png"
 export default function Header(){
     return (
         <div id="header">
-            <img src={logo} style={{position:"absolute", right:30, top: 20}} />
+            <div className="row">
+                <div style={{position:"relative"}} className="col-lg-8 col-md-9 col-sm-12">
+                    <div id="title">
+                        <h3>Moklet Laundry</h3>
+                        <h4>Terdepan, Aktual, dan Terpercaya</h4>
+                    </div>
+                </div>
+                <div style={{position:"relative"}} className="col-lg-4 col-md-3 col-sm-12">
+                    <img src={logo}
+                    style={{ display:"block", marginLeft:"auto", marginRight: "auto", marginTop:50 }} width="200" />
+                </div>
+            </div>
+            
         </div>
     )
 }
